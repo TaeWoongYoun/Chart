f1f55cb78a5bf2d969f46fe33a79a6df4850598c
xodnd0507 <24457545yong@gmail.com>
2024-03-12 10:40:45 +0900

    feat : Enter values ​​into a chart using js

--- chart.js
+++ chart.js
@@ -1,30 +1,18 @@
 const data = [
-    {
-        "day": "월",
-        "visitor": {"오전 10시": 200, "오후 2시": 170, "오후 4시": 180}
-    },
-    {
-        "day": "화",
-        "visitor": {"오전 10시": 230, "오후 2시": 200, "오후 4시": 280}
-    },
-    {
-        "day": "수",
-        "visitor": {"오전 10시": 270, "오후 2시": 270, "오후 4시": 320}
-    },
-    {
-        "day": "목",
-        "visitor": {"오전 10시": 290, "오후 2시": 170, "오후 4시": 380}
-    },
-    {
-        "day": "금",
-        "visitor": {"오전 10시": 320, "오후 2시": 370, "오후 4시": 380}
-    },
-    {
-        "day": "토",
-        "visitor": {"오전 10시": 230, "오후 2시": 670, "오후 4시": 280}
-    },
-    {
-        "day": "일",
-        "visitor": {"오전 10시": 220, "오후 2시": 270, "오후 4시": 280}
-    }
+    { "day": "월", "visitor": { "오전 10시": 200, "오후 2시": 170, "오후 4시": 180 } },
+    { "day": "화", "visitor": { "오전 10시": 230, "오후 2시": 200, "오후 4시": 280 } },
+    { "day": "수", "visitor": { "오전 10시": 270, "오후 2시": 270, "오후 4시": 320 } },
+    { "day": "목", "visitor": { "오전 10시": 290, "오후 2시": 170, "오후 4시": 380 } },
+    { "day": "금", "visitor": { "오전 10시": 320, "오후 2시": 370, "오후 4시": 380 } },
+    { "day": "토", "visitor": { "오전 10시": 230, "오후 2시": 670, "오후 4시": 280 } },
+    { "day": "일", "visitor": { "오전 10시": 220, "오후 2시": 270, "오후 4시": 280 } }
 ];
+function updateChart(day) {
+    const selectedData = data.find(item => item.day === day);
+
+    if (selectedData) {
+        document.getElementById("bar1").setAttribute("height", selectedData.visitor["오전 10시"]);
+        document.getElementById("bar2").setAttribute("height", selectedData.visitor["오후 2시"]);
+        document.getElementById("bar3").setAttribute("height", selectedData.visitor["오후 4시"]);
+    }
+}
