0256eaf3882217d004d64a4c9e43a7086491efd8
xodnd0507 <24457545yong@gmail.com>
2024-03-24 12:23:14 +0900

    test : Update js

--- chart.js
+++ chart.js
@@ -1,19 +1,29 @@
-const data = [
-    { "day": "월", "visitor": { "오전 10시": 200, "오후 2시": 170, "오후 4시": 180 } },
-    { "day": "화", "visitor": { "오전 10시": 230, "오후 2시": 200, "오후 4시": 280 } },
-    { "day": "수", "visitor": { "오전 10시": 270, "오후 2시": 270, "오후 4시": 320 } },
-    { "day": "목", "visitor": { "오전 10시": 290, "오후 2시": 170, "오후 4시": 380 } },
-    { "day": "금", "visitor": { "오전 10시": 320, "오후 2시": 370, "오후 4시": 380 } },
-    { "day": "토", "visitor": { "오전 10시": 230, "오후 2시": 670, "오후 4시": 280 } },
-    { "day": "일", "visitor": { "오전 10시": 220, "오후 2시": 270, "오후 4시": 280 } }
-];
+const chartData = {
+    "나이트리그": { "월": [50, 70, 0], "화": [20, 100, 0], "수": [60, 80, 0], "목": [90, 120, 0], "금": [100, 200, 0], "토": [150, 170, 0], "일": [30, 20, 0] },
+    "주말리그": { "월": [200, 170, 180], "화": [230, 200, 280], "수": [270, 270, 320], "목": [290, 170, 380], "금": [320, 370, 380], "토": [230, 670, 280], "일": [220, 270, 280] },
+    "새벽리그": { "월": [20, 40], "화": [21, 41], "수": [30, 33], "목": [32, 38], "금": [25, 29], "토": [30, 37], "일": [29, 39] }
+};
 
 function updateChart(day) {
-    const selectedData = data.find(item => item.day === day);
+    const selectedLeague = $('.selectedLeague').text().trim();
+    const selectedData = chartData[selectedLeague][day];
+    $('.bar').each(function(i) {
+        $(this).attr("height", selectedData[i]);
+    });
+}
 
-    if (selectedData) {
-        $("#bar1").attr("height", selectedData.visitor["오전 10시"]);
-        $("#bar2").attr("height", selectedData.visitor["오후 2시"]);
-        $("#bar3").attr("height", selectedData.visitor["오후 4시"]);
-    }
+function changeLeague(league) {
+    $('.league div').removeClass('selectedLeague');
+    const selectedDiv = $('.league div').eq(Object.keys(chartData).indexOf(league));
+    selectedDiv.addClass('selectedLeague');
+    if (league === "나이트리그") { setText("오후 6시", "오후 10시", ""); }
+    else if (league === "새벽리그") { setText("오전 4시", "오전 7시", ""); }
+    else { setText("오전 10시", "오후 2시", "오후 4시"); }
+    updateChart("월");
+}
+
+function setText(label1, label2, label3) {
+    $('#xLabel1').text(label1);
+    $('#xLabel2').text(label2);
+    $('#xLabel3').text(label3);
 }
